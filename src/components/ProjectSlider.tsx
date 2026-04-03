9bfa8ddaf50dda0b5ed2bc58eb2711710a3e8723
JennM2 <3mj.m20@gmail.com>
2026-04-02 23:23:57 -0400

    fix:Responsive sections fixed

--- src/components/ProjectSlider.tsx
+++ src/components/ProjectSlider.tsx
@@ -22,8 +22,7 @@ const projects: Project[] = [
     title: "Casa calle La Paz",
     location: "La Paz",
     area: "420 m²",
-    description:
-      "Residencia de 420 m² bajo principios bioclimáticos. Cantera gris, madera de encino y vidrio de piso a techo...",
+    description: "Residencia de 420 m² bajo principios bioclimáticos.",
   },
   {
     id: 2,
@@ -33,8 +32,7 @@ const projects: Project[] = [
     title: "Residencia Moderna",
     location: "Potosí",
     area: "380 m²",
-    description:
-      "Proyecto moderno con enfoque en iluminación natural y espacios abiertos. lo que buscábamos para nuestro proyecto en Potosí.",
+    description: "Proyecto moderno con enfoque en iluminación natural...",
   },
   {
     id: 3,
@@ -44,8 +42,7 @@ const projects: Project[] = [
     title: "Casa Minimalista",
     location: "Oruro",
     area: "450 m²",
-    description:
-      "Diseño minimalista con integración al entorno natural. lo que buscábamos para nuestro proyecto en Oruro.",
+    description: "Diseño minimalista con integración al entorno natural.",
   },
 ];
 
@@ -87,23 +84,10 @@ export default function ProjectSlider() {
   return (
     <>
       <div className="relative flex flex-col items-center justify-center w-full">
-        <button
-          onClick={prev}
-          className="hidden md:mt-53 md:block absolute left-6 md:left-30 z-30 rotate-180 top-1/2 -translate-y-1/2 transition-all duration-300 hover:scale-110"
-        >
-          <ArrowCircleRight />
-        </button>
-
-        <button
-          onClick={next}
-          className="hidden md:mt-53 md:block absolute right-6 md:right-30 z-30 top-1/2 -translate-y-1/2 transition-all duration-300 hover:scale-110"
-        >
-          <ArrowCircleRight />
-        </button>
-
-        <div className="flex items-center justify-center gap-4 md:gap-8 w-full">
+        <div className="flex items-center justify-between md:justify-center gap-0 md:gap-[clamp(1rem,3vw,2rem)] w-full overflow-visible px-0">
           {visible.map((project, index) => {
             const isCenter = index === 1;
+
             const realIndex =
               (active - 1 + index + projects.length) % projects.length;
 
@@ -111,66 +95,86 @@ export default function ProjectSlider() {
               <div
                 key={project.id}
                 onClick={() => setSelectedIndex(realIndex)}
-                className={`
-                  relative overflow-hidden transition-all duration-500 cursor-pointer
-
-                  ${
-                    isCenter
-                      ? "w-[90%] max-w-[420px] h-[620px] md:w-[315px] md:h-[320px] shadow-[8px_16px_16px_rgba(0,0,0,0.5)] z-10"
-                      : "hidden md:block w-[315px] h-[280px]"
-                  }
-                `}
+                className="relative flex justify-center items-center transition-all duration-500"
               >
-                <img
-                  src={project.image}
-                  className="w-full h-full object-cover"
-                />
                 {!isCenter && (
-                  <div
-                    className={`
-                      absolute inset-0
-                      ${
-                        index === 0
-                          ? "bg-[linear-gradient(90deg,rgba(245,240,232,1)_8%,rgba(245,240,232,0.8)_25%,rgba(245,240,232,0)_100%)]"
-                          : "bg-[linear-gradient(270deg,rgba(245,240,232,1)_8%,rgba(245,240,232,0.8)_25%,rgba(245,240,232,0)_100%)]"
-                      }
-                    `}
-                  />
-                )}
-
-                {isCenter && (
-                  <div className="absolute inset-0 bg-[linear-gradient(to_bottom,rgba(255,255,255,0)_0%,rgba(11,43,38,0.85)_85%)]" />
-                )}
-
-                {isCenter && (
-                  <div className="absolute bottom-4 left-4 text-white">
-                    <p className="text-[12px] text-accent-500 font-nunito italic">
-                      {project.category} — {project.year}
-                    </p>
-
-                    <h3 className="text-[18px] font-nunito text-primary-50">
-                      {project.title}
-                    </h3>
-
-                    <p className="text-[12px] text-primary-100 font-nunito italic">
-                      {project.location} {project.area}
-                    </p>
+                  <div className="block md:hidden w-[100%] max-w-[50px] aspect-[1/7] opacity-80 scale-90">
+                    <div className="w-full h-full overflow-hidden">
+                      <img
+                        src={project.image}
+                        className="w-full h-full object-cover object-center"
+                      />
+                    </div>
                   </div>
                 )}
+
+                <div
+                  className={`
+                    relative
+                    ${
+                      isCenter
+                        ? "w-[100%] max-w-[300px] aspect-[3/4] md:w-[clamp(260px,22vw,320px)] md:h-[clamp(380px,30vw,380px)] shadow-[8px_16px_16px_rgba(0,0,0,0.5)] z-10"
+                        : "hidden md:block w-[clamp(220px,18vw,300px)] h-[clamp(20px,18vw,280px)] opacity-80"
+                    }
+                  `}
+                >
+                  <div className="w-full h-full overflow-hidden">
+                    <img
+                      src={project.image}
+                      className="w-full h-full object-cover object-center"
+                    />
+                  </div>
+
+                  {!isCenter && (
+                    <div
+                      className={`
+                        absolute inset-0
+                        ${
+                          index === 0
+                            ? "bg-[linear-gradient(90deg,rgba(245,240,232,1)_8%,rgba(245,240,232,0.8)_25%,rgba(245,240,232,0)_100%)]"
+                            : "bg-[linear-gradient(270deg,rgba(245,240,232,1)_8%,rgba(245,240,232,0.8)_25%,rgba(245,240,232,0)_100%)]"
+                        }
+                      `}
+                    />
+                  )}
+
+                  {isCenter && (
+                    <>
+                      <div className="absolute inset-0 bg-[linear-gradient(to_bottom,rgba(255,255,255,0)_0%,rgba(11,43,38,0.85)_85%)]" />
+
+                      <div className="absolute bottom-4 left-4 text-white">
+                        <p className="text-[clamp(11px,1vw,13px)] text-accent-500 italic">
+                          {project.category} — {project.year}
+                        </p>
+
+                        <h3 className="text-[clamp(16px,1.5vw,20px)] font-nunito">
+                          {project.title}
+                        </h3>
+
+                        <p className="text-[clamp(11px,1vw,13px)] italic">
+                          {project.location} {project.area}
+                        </p>
+                      </div>
+                    </>
+                  )}
+                </div>
               </div>
             );
           })}
         </div>
 
-        <div className="flex md:hidden gap-6 mt-6">
+        <div className="flex justify-center items-center gap-8 md:gap-180 mt-[clamp(1.5rem,3vw,2.5rem)]">
           <button
             onClick={prev}
-            className="rotate-180 transition hover:scale-110"
+            className="rotate-180 transition hover:scale-110 active:scale-95 p-2 md:p-0"
           >
             <ArrowCircleRight />
           </button>
 
-          <button onClick={next} className="transition hover:scale-110">
+          <button
+            onClick={next}
+            className="transition hover:scale-110 active:scale-95 p-2 md:p-0"
+          >
             <ArrowCircleRight />
           </button>
         </div>
